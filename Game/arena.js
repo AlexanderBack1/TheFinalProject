6b869a71a25b372fc5cf61c1a2e187e9680e07ff
AlexanderBack1 <158446023+AlexanderBack1@users.noreply.github.com>
2024-04-23 21:59:45 +0200

    level ferdig

--- Game/arena.js
+++ Game/arena.js
@@ -14,9 +14,30 @@ let row3 = {
     endY: 150
 }
 
+
+
 function drawLevels(rows) {
-    ctx.fillStyle = "rgb(200 0 0 / 50%)"
+    ctx.fillStyle = "rgb(200 0 0 / 0%)"
     ctx.fillRect(0, rows.startY, 1000, rows.endY)
+}
+
+//linjer
+
+let line1 = {
+    startY: 150,
+}
+
+let line2 = {
+    startY: 350,
+}
+
+let line3 = {
+    startY: 590,
+}
+
+function drawLines(line) {
+    ctx.fillStyle = "rgb(21 113 69 / 100%)"
+    ctx.fillRect(0, line.startY, 1000, 10)
 }
 
 //stiger
@@ -33,7 +54,7 @@ stige2 = {
     startY: 130,
 
     endX: 80,
-    endY: 220,
+    endY: 230,
 }
 const stigeImg = document.getElementById("stige")
 
@@ -49,15 +70,19 @@ mal = {
     endY: 150
 }
 
+const goalImg = document.getElementById("banner")
 function skapMal() {
-    ctx.fillStyle = "rgb(100 100 100 / 100%)"
-    ctx.fillRect(mal.startX, mal.startY, mal.endX, mal.endY)
+    ctx.drawImage(goalImg, mal.startX, mal.startY, mal.endX, mal.endY)
 }
 
 function createArena() {
     drawLevels(row1)
     drawLevels(row2)
     drawLevels(row3)
+
+    drawLines(line1)
+    drawLines(line2)
+    drawLines(line3)
 
     skapStiger(stige2)
     skapStiger(stige1)
