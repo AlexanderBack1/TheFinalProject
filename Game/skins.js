df3f3ffcd171b00c44cd6e279cbc1fd75e873a78
AlexanderBack1 <158446023+AlexanderBack1@users.noreply.github.com>
2024-04-24 18:08:09 +0200

    skins selection fungerer bedre

--- Game/skins.js
+++ Game/skins.js
@@ -5,11 +5,12 @@ const kasperImg = document.getElementById("kasperImg")
 const torbImg = document.getElementById("torbImg")
 const jonasImg = document.getElementById("jonasImg")
 const skinImg = document.querySelectorAll(".skinImg")
+updateSkin()
 
 function updateSkin() {
     if (chosenSkin == 0) {
         playerImage = document.getElementById("anden")
-        andImg.style.border = "5px solid white"
+        andImg.style.border = "10px solid white"
     }
 
     if (chosenSkin == 1) {
@@ -24,6 +25,7 @@ function updateSkin() {
 
     if (chosenSkin == 3) {
         playerImage = document.getElementById("jonas")
+        jonasImg.style.border = "10px solid white"
     }
 
 }
@@ -31,22 +33,42 @@ function chosenAnd() {
     chosenSkin = 0
     localStorage.setItem("chosenSkin", chosenSkin)
     andImg.style.border = "10px solid white"
+    skinImg.forEach((img) => {
+        if (img.id !== "andImg") {
+          img.style.border = "0";
+        }
+      });
 }
 
 function chosenKasper() {
     chosenSkin = 1
     localStorage.setItem("chosenSkin", chosenSkin)
     kasperImg.style.border = "10px solid white"
+    skinImg.forEach((img) => {
+        if (img.id !== "kasperImg") {
+          img.style.border = "0";
+        }
+      });
 }
 
 function chosenTorb() {
     chosenSkin = 2
     localStorage.setItem("chosenSkin", chosenSkin)
     torbImg.style.border = "10px solid white"
+    skinImg.forEach((img) => {
+        if (img.id !== "torbImg") {
+          img.style.border = "0";
+        }
+      });
 }
 
 function chosenJonas() {
     chosenSkin = 3
     localStorage.setItem("chosenSkin", chosenSkin)
     jonasImg.style.border = "10px solid white"
+    skinImg.forEach((img) => {
+        if (img.id !== "jonasImg") {
+          img.style.border = "0";
+        }
+      });
 }
