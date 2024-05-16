03be6757e4309be5af31f7e471511b0f3610c8dc
AlexanderBack1 <158446023+AlexanderBack1@users.noreply.github.com>
2024-05-16 20:53:58 +0200

    nå er det 17. mai

--- leaderboard/leaderboardex.js
+++ leaderboard/leaderboardex.js
@@ -1,6 +1,13 @@
-    setTimeout(function() {
-        for (j=0; j<5; j++){
-            document.getElementById("spanP"+j).innerText = playerArr[j].name
-            document.getElementById("spanS"+j).innerText = playerArr[j].hs
-        }
-    }, 1000);
+const loading = document.getElementById("loading")
+const section = document.getElementById("section")
+
+
+setTimeout(function () {
+    for (j = 0; j < 5; j++) {
+        document.getElementById("spanP" + j).innerText = playerArr[j].name
+        document.getElementById("spanS" + j).innerText = playerArr[j].hs
+    }
+
+    section.removeChild(loading)
+}, 1500);
+
